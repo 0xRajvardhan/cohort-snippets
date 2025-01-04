a7a6d600ce9ce5c8f1c56a067a574f8c6cae84bb
CarlGustav <singhrajvardhan2003@gmail.com>
2025-01-04 23:40:49 +0530

    memoisation

--- w6.2-useMemo/src/App.jsx
+++ w6.2-useMemo/src/App.jsx
@@ -1,31 +1,27 @@
-import { useEffect, useMemo, useState } from "react"
+import { memo, useState } from "react";
 
 function App() {
-  const [counter, setCounter] = useState(0)
-  const [inputValue, setInputValue] = useState(1)
-
-  let count = useMemo(() => {
-    let finalCount = 0;
-    for (let i = 0; i <= inputValue; i++) {
-      finalCount = finalCount + i;
-    }
-    return finalCount;
-  }, [inputValue])
-
-
+  const [count, setCount] = useState(0);
+  function logSomething() {
+    console.log("child clicked");
+  }
   return (
     <>
-      <input onChange={(e) => {
-        setInputValue(e.target.value)
-      }} placeholder="find some from 1 to n" />
-      <br />
-      Sum from 1 to {inputValue} is {count}
-      <br />
-      <button onClick={() => {
-        setCounter(counter + 1)
-      }}>Counter ({counter})</button>
+      <ButtonComponent inputFunction={logSomething} />
+      <button
+        onClick={() => { setCount + 1 }}
+      >Click me {count}</button>
     </>
   )
 }
 
-export default App
+const ButtonComponent = memo(({ inputFunction }) => {
+  console.log("child render");
+  return (
+    <>
+      <button onClick={inputFunction}>Button clicked</button>
+    </>
+  )
+})
+
+export default App;
