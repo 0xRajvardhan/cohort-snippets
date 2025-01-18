e940790f88400ec5ab028010fe90544c71ecbc18
CarlGustav <singhrajvardhan2003@gmail.com>
2025-01-18 13:42:43 +0530

    optimisation

--- w7.2-contextAPI/src/components/Buttons.jsx
+++ w7.2-contextAPI/src/components/Buttons.jsx
@@ -1,9 +1,13 @@
+import { useSetRecoilState } from "recoil";
+import { countAtom } from "../store/atoms/count";
+
 export default function Buttons() {
-    const count = 0;
+    const setCount = useSetRecoilState(countAtom);
+    console.log("button rerednered");
     return (
         <>
-            <button onClick={() => setCount(count + 1)}>Increase</button>
-            <button onClick={() => setCount(count - 1)}>Decrease</button>
+            <button onClick={() => setCount(c => c + 1)}>Increase</button>
+            <button onClick={() => setCount(c => c - 1)}>Decrease</button>
         </>
     )
 }
--- w7.2-contextAPI/src/components/Count.jsx
+++ w7.2-contextAPI/src/components/Count.jsx
@@ -1,11 +1,15 @@
+import { RecoilRoot } from "recoil";
 import Buttons from "./Buttons";
 import CountRenderer from "./CountRenderer";
 
 export function Count (){
+    console.log("Count rerednered");
     return (
         <>
+        <RecoilRoot>
         <CountRenderer />
         <Buttons />
+        </RecoilRoot>
         </>
       )
 }
