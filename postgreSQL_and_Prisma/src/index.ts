5dad03c98941b7e07362796d58a5d4ed3dafd096
CarlGustav <singhrajvardhan2003@gmail.com>
2025-03-15 21:53:29 +0530

    inserting data using queries

--- postgreSQL_and_Prisma/src/index.ts
+++ postgreSQL_and_Prisma/src/index.ts
@@ -1,24 +1,21 @@
 import { Client } from "pg";
 import dotenv from "dotenv";
-
 dotenv.config();
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL, 
-});
-
-async function createUsersTable() {
-  await client.connect();
-  const result = await client.query(`
-    CREATE TABLE users (
-      id SERIAL PRIMARY KEY,
-      username VARCHAR(50) UNIQUE NOT NULL,
-      email VARCHAR(255) UNIQUE NOT NULL,
-      password VARCHAR(255) NOT NULL,
-      created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-    );
-  `);
-  console.log(result);
+async function insertData() {
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+  });
+  try {
+    await client.connect();
+    const insertQuery = `INSERT INTO users (username, email, password) VALUES ('John Doe2', 'email2@example.com', 'password123')`;
+    const res = await client.query(insertQuery);
+    console.log(`Insertion success`, res);
+  } catch (err) {
+    console.log(`Insertion error`, err);
+  } finally {
+    await client.end();
+  }
 }
 
-createUsersTable();
+insertData();
